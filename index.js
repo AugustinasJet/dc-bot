759ece50723e92fd53950bf5d3a90623d1d97ad9
Augustinas Čiuta <augustinas.ciuta@tesonet.com>
2022-07-29 19:33:10 +0300

    Add bot

---
+++ index.js
@@ -0,0 +1,48 @@
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+const cron = require('cron');
+
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!', new Date().toLocaleString());
+	const cronjob = new cron.CronJob(
+		'0 0 10,22 * * *',
+		async () => {
+			const user = client.users.fetch(process.env.DISCORD_RECIEVER_ID);
+			(await user).send('Laikas vaistukam');
+		});
+	cronjob.start();
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+	else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	}
+	else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+// client.on('message');
+// cronjob
+
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
+
+
+// const scheduledMessage = new cron.CronJob('* * * * * *', () => {
+// 	(await user).send('Laikas vaistukam')
+// });
+// scheduledMessage.start();
